7889a855d3bdd48f34b27b5a53d49f7e11bbec6d
kishanbg-lang <kkishanbg@gmail.com>
2026-05-15 23:50:20 +0530

    Delete 1_js_css_html directory

--- 1_js_css_html/one.js
+++
@@ -1,8 +0,0 @@
-  function editRole() {
-        const roleElement = document.getElementById('Role');
-        const currentRole = roleElement.textContent;
-        const newRole = prompt('Enter new role:', currentRole);
-        if (newRole !== null && newRole.trim() !== '') {
-            roleElement.textContent = newRole.trim();
-        }
-    }
